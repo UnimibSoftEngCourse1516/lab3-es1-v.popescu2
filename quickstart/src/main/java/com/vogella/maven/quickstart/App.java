484dd1f135424041fde9826b377bc9913a9b53c2
VictorDaniel92 <vic.popescu92@gmail.com>
2015-11-29 18:30:13 +0100

    Modified App.java

--- quickstart/src/main/java/com/vogella/maven/quickstart/App.java
+++ quickstart/src/main/java/com/vogella/maven/quickstart/App.java
@@ -1,13 +1,12 @@
 package com.vogella.maven.quickstart;
 
-/**
- * Hello world!
- *
- */
+import com.google.gson.Gson;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+      Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!"));
     }
 }
